ca69842c53e218db2f7daab9739c7189435ad046
Krispy125 <kburns19@rockets.utoledo.edu>
2026-01-22 00:21:40 -0500

    Added the road logic, contains lanes, congestion, etc.

---
+++ roadproject/src/main/java/com/krisburns/roadAI/model/Road.java
@@ -0,0 +1,62 @@
+package com.krisburns.roadai.model;
+
+public class Road {
+    private final String id;
+    private final RoadType type;
+    private final Intersection from;
+    private final Intersection to;
+
+    private final int lanes;
+    private final double speedLimit;  // meters a second, will probably change to mph later
+    private final double length; // meters
+
+    private int vehicleCount;
+
+    public Road(
+        String id,
+        RoadType type,
+        Intersection from,
+        Intersection to,
+        double length
+    ) {
+        this.id = id;
+        this.type = type;
+        this.from = from;
+        this.to = to;
+        this.length = length;
+        this.lanes = type.getDefaultLanes();
+        this.speedLimit = type.getDefaultSpeed();
+
+        from.addOutgoing(this);
+        to.addIncoming(this);
+    }
+
+    public double capacity() {
+        return lanes * 2000; // Vehicles an hour
+    }
+
+    public double congestionFactor() {
+        return Math.max(1.0, vehicleCount / capacity());
+    }
+
+    public double travelTime() {
+        return (length / speedLimit) * congestionFactor(); 
+    }
+
+    public void addVehicle() {
+        vehicleCount++;
+    }
+
+    public void removeVehicle() {
+        vehicleCount = Math.max(0, vehicleCount - 1);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Intersection getTo() { // Keep getting an error on this what am I doing :'(
+        return to;
+    }
+
+}
